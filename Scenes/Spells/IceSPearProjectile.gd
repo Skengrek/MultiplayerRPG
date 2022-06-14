b23e78b782d95f00bfdf25eeafcf36b8dd4224fa
Skengrek <armand.masse69@gmail.com>
2022-06-14 17:30:07 +0200

    Updated Ice Spear

--- Scenes/Spells/IceSPearProjectile.gd
+++ Scenes/Spells/IceSPearProjectile.gd
@@ -14,6 +14,7 @@ func cast(newDirection, globalPosition, data):
 	direction = newDirection
 	$Sprite.rotation_degrees = atan2(newDirection.y, newDirection.x) * 180 / PI
 	global_position = globalPosition
+	$AnimationPlayer.play('Fired')
 
 
 func _process(delta):
@@ -24,4 +25,4 @@ func _process(delta):
 func _on_projectile_body_entered(body):
 	if body.has_method("processDamage"):
 		body.processDamage(damage, modifier)
-		queue_free()
+	queue_free()
